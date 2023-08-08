e6cdc9f6fa83969335e427cbd57fab8749b82b5e
Roy Kjetil Liknes <109290908+royliknes@users.noreply.github.com>
2023-08-08 13:28:52 +0200

    added first function in javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function getComputerChoice(){
+    let randomNumber = Math.random();
+
+    if (randomNumber <1/3) {
+        return "Rock";
+    } else if (randomNumber < 2/3) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice());
