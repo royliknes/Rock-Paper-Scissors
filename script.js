a94943e2cfe3537fb1ef934db35cd8dd1514b22f
Roy Kjetil Liknes <109290908+royliknes@users.noreply.github.com>
2023-08-16 19:43:57 +0200

    second function added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function getComputerChoice(){
     let randomNumber = Math.random();
 
-    if (randomNumber <1/3) {
+    if (randomNumber < 1/3) {
         return "Rock";
     } else if (randomNumber < 2/3) {
         return "Paper";
@@ -12,3 +12,25 @@ function getComputerChoice(){
     }
 }
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") 
+    || (playerSelection === "paper" && computerSelection === "rock")
+    || (playerSelection === "scissors" && computerSelection === "paper")) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } 
+    else if (
+        (playerSelection === "scissors" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors")) {
+            return `You lose! ${computerSelection} beats ${playerSelection}`; 
+        }
+ }
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
